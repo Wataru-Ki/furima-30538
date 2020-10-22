7fe1f1fca756db133f0a4e94520b2c8dde42db66
Wataru-Ki <wataru_ru@outlook.jp>
2020-10-22 18:38:57 +0900

    RSpec(関連Gem)の導入、Userモデル単体テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name_kana, presence: true
   validates :birth_day, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  validates :family_name, presence: true, format: { with: VALID_NAME_REGEX }
+
+  VALID_KANA_REGEX = /\A[ァ-ン]+\z/
+  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
+  validates :family_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
 end
