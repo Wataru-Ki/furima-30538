91724c5eebe57616ac2991dc3bbfff2493000d94
Wataru-Ki <wataru_ru@outlook.jp>
2020-10-22 20:30:30 +0900

    itemモデルの作成

---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :introduction, presence: true
+  validates :shipping_area_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :category_id, presence: true
+  validates :item_condition_id, presence: true
+
+  belongs_to :user
+end
