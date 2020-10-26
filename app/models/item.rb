e0067269b148d165b858e52c2caeff396080aa18
Wataru-Ki <wataru_ru@outlook.jp>
2020-10-26 16:33:57 +0900

    itemモデル単体テストコード(金額の項を除く)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   validates :name, presence: true
-  validates :price, presence: true
   validates :introduction, presence: true
   validates :item_condition_id, presence: true
   validates :image, presence: true
@@ -10,13 +9,20 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :shipping_area
-  validates :shipping_area_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_area, presence: true
+  validates :shipping_area_id, numericality: { other_than: 1 }
   belongs_to_active_hash :shipping_date
-  validates :shipping_date_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_date, presence: true
+  validates :shipping_date_id, numericality: { other_than: 1 }
   belongs_to_active_hash :shipping_charge
-  validates :shipping_charge_id, presence: true, numericality: { other_than: 1 }
+  validates :shipping_charge, presence: true
+  validates :shipping_charge_id, numericality: { other_than: 1 }
   belongs_to_active_hash :category
-  validates :category_id, presence: true, numericality: { other_than: 1 }
+  validates :category, presence: true
+  validates :category_id, numericality: { other_than: 1 }
   belongs_to_active_hash :item_condition
-  validates :item_condition_id, presence: true, numericality: { other_than: 1 }
+  validates :item_condition, presence: true
+  validates :item_condition_id, numericality: { other_than: 1 }
+
+  validates :price, presence: true, numericality: { only_integer: true }
 end
