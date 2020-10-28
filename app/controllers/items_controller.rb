03e2bda9197fb5e06ff62c12c4f5c96b90434c73
Wataru-Ki <wataru_ru@outlook.jp>
2020-10-28 18:32:00 +0900

    商品詳細表示機能・修正
    ①コントローラーをbefore_actionでまとめた②詳細画面の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_item, only: :show
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,11 +20,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :price, :introduction, :shipping_area_id, :shipping_date_id, :shipping_charge_id, :category_id, :item_condition_id, :image).merge(user_id: current_user.id)
