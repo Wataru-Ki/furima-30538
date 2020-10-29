5a7a8b426ac47d1e11b12a431248bb60097900cb
Wataru-Ki <wataru_ru@outlook.jp>
2020-10-29 16:08:52 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: :show
+  before_action :set_item, only: [:show, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,6 +20,14 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
 
