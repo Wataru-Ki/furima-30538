6bc086c4c74b64e3e67eae845717d643b57735fa
Wataru-Ki <wataru_ru@outlook.jp>
2020-10-29 15:05:57 +0900

    itemコントローラーの修正
    URLを直接入力した際のredirectの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -42,5 +43,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :price, :introduction, :shipping_area_id, :shipping_date_id, :shipping_charge_id, :category_id, :item_condition_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user.id
+      redirect_to action: :index
+    end
   end
 end
